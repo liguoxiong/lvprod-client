ea09eabadd6623bb7ace491d83c1c21047a0e2da
hunglq <hunglq@vietcas.vn>
2019-09-11 15:21:21 +0700

    Updates

---
+++ precache-manifest.3be934ef59f4010b98c1990118de1b9d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1f7b2fe72627d1ecaaf314e2d43be86b",
+    "url": "/lvprod-client/index.html"
+  },
+  {
+    "revision": "2d3dcbb12be10135c721",
+    "url": "/lvprod-client/static/css/2.ef94d21c.chunk.css"
+  },
+  {
+    "revision": "5fd7fe57c475428c5b9f",
+    "url": "/lvprod-client/static/css/main.0e5e16a6.chunk.css"
+  },
+  {
+    "revision": "2d3dcbb12be10135c721",
+    "url": "/lvprod-client/static/js/2.a1d3e1df.chunk.js"
+  },
+  {
+    "revision": "5fd7fe57c475428c5b9f",
+    "url": "/lvprod-client/static/js/main.20389757.chunk.js"
+  },
+  {
+    "revision": "f83df487ac0b61e9d830",
+    "url": "/lvprod-client/static/js/runtime~main.85a80587.js"
+  }
+]);
