698816199b892b0e00b9d13e56844923d3e62da1
hunglq <hunglq@vietcas.vn>
2019-09-17 16:24:56 +0700

    update

--- src/router.js
+++ src/router.js
@@ -1,12 +1,10 @@
 import React from "react";
 import { BrowserRouter as Router, Route } from "react-router-dom";
-import Login from "./Components/login/login.view";
-// import Home from "./Components/index";
+import Home from "./Components/index";
 function AppRouter() {
   return (
     <Router>
-      <Route path="/login" component={Login} />
-      {/* <Route exact path="/" component={Home} /> */}
+      <Route exact path="/" component={Home} />
     </Router>
   );
 }
