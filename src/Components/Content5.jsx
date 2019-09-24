41ef455dcc80de7ea043eaca4811a948d2d43a5f
hunglq <hunglq@vietcas.vn>
2019-09-24 11:20:08 +0700

    update

--- src/Components/Content5.jsx
+++ src/Components/Content5.jsx
@@ -1,30 +1,58 @@
-import React from "react";
+import React, {Component} from "react";
 import { Row, Col, Button } from "antd";
 import { TweenOneGroup } from "rc-tween-one";
 import OverPack from "rc-scroll-anim/lib/ScrollOverPack";
 import { getChildrenToRender } from "./utils";
+import axios from "axios";
 
-class Content5 extends React.PureComponent {
+class Content5 extends React.Component {
+
+  state = {
+    Products: [],
+  }
+  componentDidMount() {
+    axios
+      .all([
+        axios.get("/api/products?limit=8"),
+      ])
+      .then(
+        axios.spread((products) => {
+          this.setState({
+            catActive: '',
+            Products: products.data.data
+          });
+        })
+      );
+  }
   getChildrenToRender = data =>
     data.map((item, i) => {
       return (
         <Col key={i.toString()} className="block" md={6} xs={24}>
           <div className="content5-block-content">
             <span>
-              <img src={item.img} height="100%" alt="img" />
+              <img src={item.image[0].thumbUrl} height="100%" alt="img" />
             </span>
-            <p>{item.content}</p>
+            <p>{item.name}</p>
           </div>
         </Col>
       );
     });
+
+    handleSelectCategory = id => {
+      let query='limit=8';
+      if (id!=='') {
+        query = `category=${id}&limit=8`
+      } 
+      axios.get(`/api/products?${query}`)
+        .then(res => this.setState({catActive: id, Products: res.data.data}))
+    }
 
   render() {
     const { ...props } = this.props;
     const { dataSource } = props;
     delete props.dataSource;
     delete props.isMobile;
-    const childrenToRender = this.getChildrenToRender(dataSource.block);
+    const childrenToRender = this.getChildrenToRender(this.state.Products);
     return (
       <div
         id="Content5_0"
@@ -33,12 +61,24 @@ class Content5 extends React.PureComponent {
       >
         <div className="home-page content5">
           <div key="title" className="title-wrapper">
-            {dataSource.titleWrapper.map(getChildrenToRender)}
+            <h1 className="title-h1">SẢN PHẨM</h1>
+            <div className="title-content">Chúng tôi tự hào cung cấp các sản phẩm thế mạnh</div>
           </div>
           <div key="categoryList" className="category-wrapper">
-            <Button type="primary">Tất cả</Button>
-            {dataSource.categoriesList.map(item => (
-              <Button>{item.title}</Button>
+            <Button 
+              type={this.state.catActive==='' ? "primary" : null}
+              onClick={() => this.handleSelectCategory('')}
+              >
+                Tất cả
+            </Button>
+            {!!dataSource && dataSource.map(item => (
+              <Button 
+                key={item._id}
+                type={this.state.catActive===item._id ? "primary" : null}
+                onClick={() => this.handleSelectCategory(item._id)}
+              >
+                {item.title}
+              </Button>
             ))}
           </div>
           <OverPack className="content-template" playScale={0.3}>
