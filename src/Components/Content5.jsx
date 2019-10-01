760526e18df0b96f9cab1d04796b769edb45c498
liguoxiong <bunyhung@gmail.com>
2019-10-01 19:16:02 +0700

    update

--- src/Components/Content5.jsx
+++ src/Components/Content5.jsx
@@ -1,4 +1,4 @@
-import React, {Component} from "react";
+import React, { Component } from "react";
 import { Row, Col, Button } from "antd";
 import { TweenOneGroup } from "rc-tween-one";
 import OverPack from "rc-scroll-anim/lib/ScrollOverPack";
@@ -6,23 +6,18 @@ import { getChildrenToRender } from "./utils";
 import axios from "axios";
 
 class Content5 extends React.Component {
-
   state = {
-    Products: [],
-  }
+    Products: []
+  };
   componentDidMount() {
-    axios
-      .all([
-        axios.get("/api/products?limit=8"),
-      ])
-      .then(
-        axios.spread((products) => {
-          this.setState({
-            catActive: '',
-            Products: products.data.data
-          });
-        })
-      );
+    axios.all([axios.get("/api/products?isShow=true&limit=8")]).then(
+      axios.spread(products => {
+        this.setState({
+          catActive: "",
+          Products: products.data.data
+        });
+      })
+    );
   }
   getChildrenToRender = data =>
     data.map((item, i) => {
@@ -30,7 +25,11 @@ class Content5 extends React.Component {
         <Col key={i.toString()} className="block" md={6} xs={24}>
           <div className="content5-block-content">
             <span>
-              <img src={item.image[0].thumbUrl || item.image[0].url} height="100%" alt="img" />
+              <img
+                src={item.image[0].thumbUrl || item.image[0].url}
+                height="100%"
+                alt="img"
+              />
             </span>
             <p>{item.name}</p>
           </div>
@@ -38,14 +37,15 @@ class Content5 extends React.Component {
       );
     });
 
-    handleSelectCategory = id => {
-      let query='limit=8';
-      if (id!=='') {
-        query = `category=${id}&limit=8`
-      } 
-      axios.get(`/api/products?${query}`)
-        .then(res => this.setState({catActive: id, Products: res.data.data}))
+  handleSelectCategory = id => {
+    let query = "isShow=true&limit=8";
+    if (id !== "") {
+      query = `category=${id}&isShow=true&limit=8`;
     }
+    axios
+      .get(`/api/products?${query}`)
+      .then(res => this.setState({ catActive: id, Products: res.data.data }));
+  };
 
   render() {
     const { ...props } = this.props;
@@ -62,24 +62,27 @@ class Content5 extends React.Component {
         <div className="home-page content5">
           <div key="title" className="title-wrapper">
             <h1 className="title-h1">SẢN PHẨM</h1>
-            <div className="title-content">Chúng tôi tự hào cung cấp các sản phẩm thế mạnh</div>
+            <div className="title-content">
+              Chúng tôi tự hào cung cấp các sản phẩm thế mạnh
+            </div>
           </div>
           <div key="categoryList" className="category-wrapper">
-            <Button 
-              type={this.state.catActive==='' ? "primary" : null}
-              onClick={() => this.handleSelectCategory('')}
-              >
-                Tất cả
+            <Button
+              type={this.state.catActive === "" ? "primary" : null}
+              onClick={() => this.handleSelectCategory("")}
+            >
+              Tất cả
             </Button>
-            {!!dataSource && dataSource.map(item => (
-              <Button 
-                key={item._id}
-                type={this.state.catActive===item._id ? "primary" : null}
-                onClick={() => this.handleSelectCategory(item._id)}
-              >
-                {item.title}
-              </Button>
-            ))}
+            {!!dataSource &&
+              dataSource.map(item => (
+                <Button
+                  key={item._id}
+                  type={this.state.catActive === item._id ? "primary" : null}
+                  onClick={() => this.handleSelectCategory(item._id)}
+                >
+                  {item.title}
+                </Button>
+              ))}
           </div>
           <OverPack className="content-template" playScale={0.3}>
             <TweenOneGroup
