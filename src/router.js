a7dd363783b695fe1da0c0ec327b4ea5a69d0a9b
hunglq <hunglq@vietcas.vn>
2019-10-03 09:10:51 +0700

    update

--- src/router.js
+++ src/router.js
@@ -1,10 +1,12 @@
 import React from "react";
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import Home from "./Components/index";
+import ProductPage from './Components/ProductPage'
 function AppRouter() {
   return (
     <Router>
       <Route exact path="/" component={Home} />
+      <Route exact path="/product" component={ProductPage} />
     </Router>
   );
 }
